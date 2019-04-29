5c1a7092abe54be5004c85eafdfab9d093646a0a
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-04-29 23:00:01 +0200

    modified files

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,42 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeJsPlugin = require('optimize-js-plugin');
+const webpack = require('webpack');
 
 //webpack.config.js
 module.exports = (env) => {
     const environment = env || 'production';
 
-    const plugins = [new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    })];
-
-    if (env === 'production') {
-        plugins.push(
-            new OptimizeJsPlugin({
-                sourceMap: false
-            })
-        )
-    }
-
     return {
         mode: environment,
         entry: './src/index.js',
         output: {
-            path: path.resolve(__dirname, 'docs'),
-            filename: 'app.' + environment + '.bundle.js'
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
         },
+        //bez tego nie widzi reacta - nie dołącza reacta do pliku app.bundle.js
+        plugins: [
+            new webpack.ProvidePlugin({
+                "React": "react",
+            }),
+        ],
         module: {
             rules: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [{
-                    loader: 'style-loader'
+                    test: /\.js$/,
+                    loader: "babel-loader"
                 },
                 {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true
-                    }
+                    test: /\.css$/,
+                    use: [{
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
                 }
-                ]
-            }
             ]
-        },
-        plugins
+        }
     }
 };
